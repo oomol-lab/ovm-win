5bfc4d06b6073b9ca1a3466a0a681a726e5facaa
ihexon <14349453+ihexon@users.noreply.github.com>
2024-06-08 16:48:15 -0700

    feat(ovm): support un-archive zstd

---
+++ pkg/util/decompressor/archiver.go
@@ -0,0 +1,85 @@
+package archiver
+
+import (
+	"fmt"
+	"github.com/oomol-lab/ovm-win/pkg/util"
+	"io"
+	"os"
+)
+
+// Decompressor needs to implement the Compression method and register itself through TExt
+type decompressor interface {
+	decompress(in io.Reader, out io.Writer) error
+}
+
+// DecompressFile decompress a compressed file through file based IO
+func DecompressFile(source, destination string, overwrite bool) error {
+	iface, err := createWithExt(source)
+	if err != nil {
+		return err
+	}
+
+	decompressor, ok := iface.(decompressor)
+	if !ok {
+		return fmt.Errorf("format specified by source filename is not a recognized compression algorithm: %s", source)
+	}
+
+	return fileCompressor{
+		decompressor: decompressor,
+		overwrite:    overwrite,
+	}.decompressFile(source, destination)
+}
+
+type extensionChecker interface {
+	checkExt(name string) error
+}
+
+// ExtensionChecker every ExtensionChecker has chance to get execute
+var extCheckers = []extensionChecker{
+	&zst{},
+}
+
+// ByExt return the interface corresponding to the suffix name
+func createWithExt(filename string) (decompressor, error) {
+	var ifce interface{}
+	for _, obj := range extCheckers {
+		if err := obj.checkExt(filename); err == nil {
+			ifce = obj
+			break
+		}
+	}
+
+	switch ifce.(type) {
+	case *zst:
+		return newZst(), nil
+	default:
+		return nil, fmt.Errorf("format unrecognized by filename: %s", filename)
+	}
+}
+
+// fileCompressor used to un-archive a FILE BASE archives
+type fileCompressor struct {
+	decompressor
+	// Whether to overwrite existing files when creating files.
+	overwrite bool
+}
+
+func (fc fileCompressor) decompressFile(source, destination string) error {
+	if err := util.Exists(destination); !os.IsNotExist(err) && !fc.overwrite {
+		return fmt.Errorf("file already exists: %s", destination)
+	}
+
+	in, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	return fc.decompress(in, out)
+}
---
+++ pkg/util/decompressor/zstd.go
@@ -0,0 +1,42 @@
+package archiver
+
+import (
+	"fmt"
+	"github.com/klauspost/compress/zstd"
+	"io"
+	"path/filepath"
+)
+
+type zst struct{}
+
+func newZst() *zst {
+	return &zst{}
+}
+
+func (z *zst) decompress(in io.Reader, out io.Writer) error {
+	readed, _ := z.openReader(in)
+	defer readed.Close()
+	if _, err := io.Copy(out, readed); err != nil {
+		return fmt.Errorf("failed to decompress zstd stream: %w", err)
+	}
+
+	return nil
+}
+
+func (z *zst) openReader(in io.Reader) (io.ReadCloser, error) {
+	zr, err := zstd.NewReader(in)
+	if err != nil {
+		return nil, fmt.Errorf("failed to new zstd reader stream: %w", err)
+	}
+
+	return io.NopCloser(zr), nil
+}
+
+func (z *zst) checkExt(filename string) error {
+	ext := filepath.Ext(filename)
+	if ext != ".zst" && ext != ".zstd" {
+		return fmt.Errorf("filename %s must have a .zst extension", filename)
+	}
+
+	return nil
+}
