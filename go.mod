1e27ecf5cb73fa09e4860eb021c73338f99c2d07
ihexon <14349453+ihexon@users.noreply.github.com>
2024-06-08 16:38:29 -0700

    feat(ovm): support extract zstd rootfs

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Code-Hex/go-infinity-channel v1.0.0
 	github.com/Microsoft/go-winio v0.6.2
 	github.com/hashicorp/go-version v1.7.0
+	github.com/klauspost/compress v1.17.8
 	golang.org/x/sync v0.7.0
 	golang.org/x/sys v0.20.0
 )
---
+++ pkg/util/ztsd.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"github.com/klauspost/compress/zstd"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func Decompress(in string, out string) error {
+	file, err := os.Open(in)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to open input file: %v", err))
+	}
+	defer file.Close()
+
+	outputFile := filepath.Join(out)
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to create output file: %v", err))
+	}
+	defer outFile.Close()
+
+	zs := NewZstd()
+	err = zs.decompress(file, outFile)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed to decompress file: %v", err))
+	}
+
+	return nil
+}
+
+type Zstd struct {
+	DecoderOptions []zstd.DOption
+}
+
+// Decompress reads in, decompresses it, and writes it to out.
+func (zs *Zstd) decompress(in io.Reader, out io.Writer) error {
+	r, err := zstd.NewReader(in, zs.DecoderOptions...)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	_, err = io.Copy(out, r)
+	return err
+}
+
+// NewZstd returns a new, default instance ready to be customized and used.
+func NewZstd() *Zstd {
+	return new(Zstd)
+}
