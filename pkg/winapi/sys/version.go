45eb0f49e6c381dc557528dd305a18587cde286b
Kevin Cui <bh@bugs.cc>
2025-03-27 10:25:54 +0800

    refactor(sys)!: set min windows version to 19044 (#102)
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- pkg/winapi/sys/version.go
+++ pkg/winapi/sys/version.go
@@ -9,9 +9,11 @@ import (
 )
 
 // 19043 == 21H1
-// Although Microsoft claims that 19041 supports WSL2, it was actually supported in subsequent updates,
-// not from the beginning. Version 19043 supports WSL2 from the start. For convenience, 19043 is used here.
-const minBuildNumber uint32 = 19043
+// 19044 == 21H2
+// Although Microsoft claims that version 19041 supports WSL2, it was actually supported in later updates, not right from the start.
+// Version 19043 supports WSL2 from the beginning, but tests show that the latest version of WSL2 has issues when used on 19043.
+// Therefore, for convenience, version 19044 is used here.
+const minBuildNumber uint32 = 19044
 
 func SupportWSL2(log *logger.Context) bool {
 	v := windows.RtlGetVersion()
